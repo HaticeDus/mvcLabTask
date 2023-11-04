d783d54caac7e36cb889d4d2d9e4a3fe060cb113
Coders Club <dusgulsum@gmail.com>
2023-11-04 21:07:58 +0300

    lab_1

---
+++ Laboratory Work 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Laboratory_Work_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
